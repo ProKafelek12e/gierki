0ca19d76fa1a9d914a147ec6c82ba3efd6e2e840
ProKafelek12e <pro.kafelek@gmail.com>
2024-11-05 19:00:32 +0100

    ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        addres: '192.168.1.47',
+      },
     images:{
-        domains:['172.16.15.134']
+        domains:['172.16.15.134','192.168.1.47']
     }
 };
 
